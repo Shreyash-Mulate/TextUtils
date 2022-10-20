9d3703589312c49a3f9ad44938091699b06b8309
Shreyash <shreyashmulate@gmail.com>
2022-10-20 21:36:23 +0530

    added copy, bold, underline, rm extra space feature

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -10,6 +10,34 @@ export default function TextForm(props) {
         let lower = text.toLowerCase();
         setText(lower);
     }
+    const handleBoldClick = () =>{
+        let bold = text;
+        let boldTag = document.createElement("b");
+        boldTag.innerHTML = bold;
+        document.querySelector(".preview").innerHTML = '';
+        document.querySelector(".preview").appendChild(boldTag);
+    }
+    const handleUnderlineClick = () =>{
+        let underlineTxt = text;
+        let underlineTag = document.createElement("u");
+        underlineTag.innerHTML = underlineTxt;
+        document.querySelector(".preview").innerHTML = '';
+        document.querySelector(".preview").appendChild(underlineTag);
+    }
+    const handleRmSpaceClick = () =>{
+        let newText = text.replace(/\s+/g, ' ').trim();
+        setText(newText);
+    }
+    const handleCopyClick = () =>{
+        let copyText = document.querySelector("textarea");
+        copyText.select();
+        navigator.clipboard.writeText(copyText.value);
+        alert("Copied message: "+copyText.value);
+    }
+    const handleClearClick = () => {
+        let clear = "";
+        setText(clear);
+    }
     const handleOnChange = (event) => {
         // console.log("Handle on change");
         setText(event.target.value);
@@ -26,15 +54,19 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick} >Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowClick} >Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleBoldClick} >Convert to bold</button>
+            <button className="btn btn-primary mx-2" onClick={handleUnderlineClick} >Underline</button>
+            <button className="btn btn-primary mx-2" onClick={handleRmSpaceClick} >Remove leading and trailing spaces</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopyClick} >Copy</button>
+            <button className="btn btn-primary mx-2" onClick={handleClearClick} >Clear all</button>
         </div>
         <div className="container my-3">
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} words {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p className="preview">{text}</p>
         </div>
         </>
-
     )
 }
