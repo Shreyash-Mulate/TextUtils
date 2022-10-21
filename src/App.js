f045c44bd7ab8c0e78f77ab4a450251877906677
Shreyash <shreyashmulate@gmail.com>
2022-10-21 21:57:21 +0530

    Enhanced dark mode feature

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 // import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');  // whether dark mode is enabled or not
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container">
-        <TextForm heading="Enter the text to analyze"/>
+        <TextForm heading="Enter the text to analyze" mode={mode}/>
         {/* <About/> */}
       </div>
     </>
