5669c1cc97743291df9edc8ad54a29e09db3a5ca
Shreyash-Mulate <shreyashmulate@gmail.com>
2022-10-25 19:48:50 +0530

    React-router setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import './App.css';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');  // whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
     setAlert({
-      msg : message,
-      type : type
+      msg: message,
+      type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
   }
   const toggleMode = () => {
-    if(mode === 'light'){
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
       document.title = 'TextUtils - Dark Mode';
     }
-    else{
+    else {
       setMode("light");
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
@@ -35,12 +41,18 @@ function App() {
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            {/* /users --> Component 1
+            /users/home --> Component 2      reason why exact path is used instead of path*/}
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
